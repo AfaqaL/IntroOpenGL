70219f7004dca2c766cdce071b92314d57507c5a
AfaqaL <lapak18@freeuni.edu.ge>
2020-08-08 23:06:46 +0400

    Basic abstraction, preparation for testing

--- IntroOpenGL/Source/Application.cpp
+++ IntroOpenGL/Source/Application.cpp
@@ -6,182 +6,234 @@
 #include <string>
 #include <sstream>
 
-struct ShaderSrc {
-    std::string VertexSrc;
-    std::string FragmentSrc;
-};
+#include"Renderer.h"
+#include"IndexBuffer.h"
+#include"VertexBuffer.h"
+#include"VertexBufferLayout.h"
+#include"VertexArray.h"
+#include"Shader.h"
+#include"Texture.h"
 
-static ShaderSrc ParseShader(const std::string& filePath) {
-    std::ifstream input(filePath);
+#include"glm/glm.hpp"
+#include"glm/gtc/matrix_transform.hpp"
 
-    enum class ShaderType {
-        NONE = -1, VERTEX = 0, FRAGMENT = 1
-    };
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_glfw.h"
 
-    std::string line;
-    std::stringstream ss[2];
-    ShaderType type = ShaderType::NONE;
-
-    while (getline(input, line)) {
-        if (line.find("#shader") != std::string::npos) {
-            if (line.find("vertex") != std::string::npos) {
-                type = ShaderType::VERTEX;
-            }
-            else if(line.find("fragment") != std::string::npos){
-                type = ShaderType::FRAGMENT;
-            }
-        }
-        else {
-            ss[(int)type] << line << '\n';
-        }
-    }
-    return { ss[0].str(), ss[1].str() };
-}
-
-static unsigned int CompileShader(unsigned int type, const std::string& str_src) {
-    unsigned int id = glCreateShader(type);
-    const char* src = str_src.c_str();
-    glShaderSource(id, 1, &src, nullptr);
-    glCompileShader(id);
-
-    /* debug code ! */
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-    if (result == GL_FALSE) {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = (char*)_alloca(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "Failed to compile " << std::endl << message << std::endl;
-        glDeleteShader(id);
-        return 0;
-    }
-
-
-    return id;
-}
-
-static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
-    unsigned int program = glCreateProgram();
-    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
-    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
-
-    glAttachShader(program, vs);
-    glAttachShader(program, fs);
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vs);
-    glDeleteShader(fs);
-
-    return program;
+static void ChangeColor(float* red_val, int& idx, float& diff) {
+	if (red_val[idx] > 1.0f) {
+		++idx;
+		if (idx % 3 == 0) {
+			idx = 0;
+			diff = -0.02f;
+		}
+	}
+	else if (red_val[idx] < 0.0f) {
+		++idx;
+		if (idx % 3 == 0) {
+			idx = 0;
+			diff = 0.02f;
+		}
+	}
+	red_val[idx] += diff;
 }
 
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+	
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(960, 540, "Baro Lasha", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	if (glewInit() != GLEW_OK)
+		std::cout << "ERROR!\n";
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
-    
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Baro Lasha", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+#pragma region My stuff after initialization
+	{
+		float positions[] = {
+			-50.0f, -50.0f, 0.0f, 0.0f,
+			 50.0f, -50.0f, 1.0f, 0.0f,
+			 50.0f,  50.0f, 1.0f, 1.0f,
+			-50.0f,  50.0f, 0.0f, 1.0f
+		};
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+		unsigned int indicies[] = {
+			0, 1, 2,
+			2, 3, 0
+		};
 
-    if (glewInit() != GLEW_OK)
-        std::cout << "ERROR!\n";
+#pragma region crown points
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
+		float crown_pos[] = {
+			-0.3f, 0.5f, //left-down 0
+			 0.0f, 0.95f, //top-mid 1
+			 0.3f, 0.5f, //right-down 2
+			-0.5f, 0.85f, // left-top 3
+			 0.5f, 0.85f //top_right 4
+		};
 
-    float positions[] = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.5f,  0.5f,
-        -0.5f,  0.5f,
-    };
+		unsigned int crown_indicies[] = {
+			0, 3, 2,
+			0, 1, 2,
+			0, 4, 2
+		};
 
+		float red_val[] = {
+			0.0f, 0.0f, 0.0f
+		};
+		int idx = 0;
+		float diff = 0.02f;
+		
+#pragma endregion
+		
+		/* blending */
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-    /* generate a vertex buffer */
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		VertexArray va;
+		VertexArray cr_va;
 
-    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+		VBufferLayout layout;
+		layout.Push<float>(2);
+		layout.Push<float>(2);
+		va.AddBuffer(vb, layout);
+		
+		VertexBuffer cr_vb(crown_pos, 5 * 2 * sizeof(float));
+		VBufferLayout cr_layout;
+		cr_layout.Push<float>(2);
+		cr_va.AddBuffer(cr_vb, cr_layout);
+		
+		IndexBuffer cr_ib(crown_indicies, 9);
+		
+		Shader cr_shader("Resources/Shaders/Crown.shader");
+		cr_shader.Bind();
 
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+		IndexBuffer ib(indicies, 6);
 
-    unsigned int indicies[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+		
 
-    glfwSwapInterval(1);
+		Shader shader("Resources/Shaders/Basic.shader");
+		shader.Bind();
+		Texture texture("Resources/Textures/nino.png");
+		texture.Bind();
+		shader.SetUniform1i("u_Texture", 0);
+		
+		va.Unbind();
+		shader.Unbind();
+		vb.Unbind();
+		ib.Unbind();
 
-    /* generate index buffer */
-    unsigned int idx_buff;
-    glGenBuffers(1, &idx_buff);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idx_buff);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indicies, GL_STATIC_DRAW);
+		Renderer renderer;
 
-    ShaderSrc source = ParseShader("Source/Resources/Basic.shader");
-    
+		/* >>>>>>>>>>>>>>>>>>>>>
+			!!!!! Setting IMGUI !!!!
+			<<<<<<<<<<<<<<<<<<<< */
 
-    unsigned int shader = CreateShader(source.VertexSrc, source.FragmentSrc);
-    glUseProgram(shader);
+		ImGui::CreateContext();
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init((const char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
+		ImGui::StyleColorsDark();
 
-    int u_id = glGetUniformLocation(shader, "u_Color");
+		glm::vec3 translation = glm::vec3(200, 200, 0);
+		glm::vec3 translation2 = glm::vec3(400, 200, 0);
 
-    float red_val[] = {
-        0.0f, 0.0f, 0.0f
-    };
-    int idx = 0;
-    float diff = 0.02f;
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+		bool show_demo_window = true;
+		bool show_another_window = false;
+		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-        
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* Render here */
+			renderer.Clear();
 
-        glUniform4f(u_id, red_val[0], red_val[1], red_val[2], 1.0f);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
 
-        if (red_val[idx] > 1.0f) {
-            ++idx;
-            if (idx % 3 == 0) {
-                idx = 0;
-                diff = -0.02f;
-            }
-        }
-        else if (red_val[idx] < 0.0f) {
-            ++idx;
-            if (idx % 3 == 0) {
-                idx = 0;
-                diff = 0.02f;
-            }
-        }
-        red_val[idx] += diff;
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+			ImGui::NewFrame();
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f), translation2);
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4("u_MVP", mvp);
+				renderer.Draw(va, ib, shader);
+			}
 
+			vb.Bind();
+			
+			cr_shader.Bind();
+			cr_shader.SetUniform4f("u_Color", clear_color.x, 
+				clear_color.y, clear_color.z, clear_color.w);
+			cr_vb.Bind();
+			renderer.Draw(cr_va, cr_ib, cr_shader);
 
-    glfwTerminate();
-    return 0;
+			ChangeColor(red_val, idx, diff);
+
+			{
+				//static float f = 0.0f;
+				//static int counter = 0;
+
+				//ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				//ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				//ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+				//ImGui::Checkbox("Another Window", &show_another_window);
+
+				ImGui::SliderFloat3("Translation A", &translation.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::SliderFloat3("Translation B", &translation2.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				//if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+				//	counter++;
+				//ImGui::SameLine();
+				//ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				//ImGui::End();
+			}
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
+
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
+
+	}
+
+#pragma endregion
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+
+	ImGui::DestroyContext();
+
+	glfwTerminate();
+	return 0;
 }
